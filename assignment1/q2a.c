29da95f2f6b34d221ad612cb6f48c4ab925c85a8
Eric McEvoy <eric.mcevoy95@gmail.com>
2018-03-19 23:23:16 +0000

    changed stopinit to stop-init

--- assignment1/q2a.c
+++ assignment1/q2a.c
@@ -1,7 +1,39 @@
 #include <stdio.h>
-int main()
+#include <time.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+int main(int argc, char** argv)
 {
-   // printf() displays the string inside quotation
-   printf("Hello, World!");
-   return 0;
-}
+
+ struct timeval tv;
+ struct timezone tz;
+ int i,delay,num_iter;
+ double init,start,stop,stopinit;
+
+ if (argc!=3)
+ {
+ fprintf(stderr, "Usage: %s <sleep time..msec><num_iteration>\n", argv[0]);
+ exit(1);
+ }
+ // progname=argv[0];
+ delay=atoi(argv[1]);
+ num_iter=atoi(argv[2]);
+ printf("Delay is %d..num_iter is %d\n",delay,num_iter);
+ gettimeofday( &tv,&tz);
+ init=tv.tv_sec + tv.tv_usec*0.000001;
+ for(i=0;i<num_iter;++i)
+ {
+ gettimeofday( &tv,&tz);
+ start=tv.tv_sec + tv.tv_usec*0.000001;
+ // Now sleep
+ usleep(delay*1000);
+ gettimeofday( &tv,&tz);
+ stop=tv.tv_sec + tv.tv_usec*0.000001;
+ printf("Time is %ld : %d.slept for %lf ms\n",tv.tv_sec,tv.tv_usec,(stop-start)*1000);
+ }
+ printf("Total time taken : actual %lf theory(excl. runtime): %d, ms \n",(stop-init)*1000,num_iter*delay);
+
+ return 0;
+} 
+
